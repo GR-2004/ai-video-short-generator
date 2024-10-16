256599dc59a6b4f0b10ee06f4bfdfbcebf6e4f89
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-10-16 09:13:57 +0530

    add clerk authentication

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Provider from "./Provider.jsx";
+import { ClerkProvider } from "@clerk/nextjs";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -19,12 +22,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
