81575225e9415831bdbe6987498f25a39a6ee732
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-10-16 18:15:12 +0530

    add create-new page

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import SelectTopic from './_components/SelectTopic'
+import SelectStyle from './_components/SelectStyle';
+import SelectDuration from './_components/SelectDuration';
+import { Button } from '@/components/ui/button';
 
 const CreateNewPage = () => {
+  const [formData, setFormData] = useState([]);
+  const onHandleInputChange = (fieldName, fieldValue) => {
+    setFormData(prev => ({
+      ...prev,
+      [fieldName]: fieldValue
+    }))
+  }
   return (
-    <div>
-      CreateNewPage
+    <div className='md:px-20'>
+        <h2 className='font-bold text-4xl text-primary text-center'>Create New</h2>
+        <div className='mt-10 shadow-lg p-10'>
+          {/* Select Topic */}
+          <SelectTopic onUserSelect={onHandleInputChange} />
+          {/* Select Style */}
+          <SelectStyle onUserSelect={onHandleInputChange} />
+          {/* Duration */}
+          <SelectDuration onUserSelect={onHandleInputChange} />
+          {/* Create Button */}
+          <Button className="mt-10 w-full text-xl">Create Short Video</Button>
+        </div>
     </div>
   )
 }
