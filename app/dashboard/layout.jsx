6b314244912e033b4319f5e7ad1113eb6b6c6c3f
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-10-19 21:33:31 +0530

    add videoData Context

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Header from "./_components/Header";
 import SideNav from "./_components/SideNav";
+import { VideoDataContext } from "../_context/VideoDataContext";
 
 const DashboardLayout = ({ children }) => {
+  const [videoData, setVideoData] = useState([]);
   return (
+    <VideoDataContext.Provider value={{videoData, setVideoData}}>
     <div>
       <div className="hidden md:block h-screen bg-white fixed mt-[65px] w-64">
         <SideNav />
@@ -13,6 +17,7 @@ const DashboardLayout = ({ children }) => {
         <div className="md:ml-64 p-10">{children}</div>
       </div>
     </div>
+    </VideoDataContext.Provider>
   );
 };
 
