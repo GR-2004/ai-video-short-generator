755091af7f0977d5c6a7d4402185f5c8831c08bc
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-10-19 21:36:54 +0530

    add generateImage function

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import SelectTopic from "./_components/SelectTopic";
 import SelectStyle from "./_components/SelectStyle";
 import SelectDuration from "./_components/SelectDuration";
@@ -7,6 +7,8 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import CustomLoading from "./_components/CustomLoading";
 import { v4 as uuidv4 } from "uuid";
+import { VideoDataContext } from "@/app/_context/VideoDataContext";
+import { useEffect } from "react";
 
 const CreateNewPage = () => {
   const [formData, setFormData] = useState([]);
@@ -14,7 +16,8 @@ const CreateNewPage = () => {
   const [videoScript, setVideoScript] = useState();
   const [audioFileUrl, setAudioFileUrl] = useState();
   const [captions, setCaptions] = useState();
-
+  const [imageList, setImageList] = useState();
+  const {videoData, setVideoData} = useContext(VideoDataContext)
   const onHandleInputChange = (fieldName, fieldValue) => {
     setFormData((prev) => ({
       ...prev,
@@ -23,60 +26,108 @@ const CreateNewPage = () => {
   };
 
   const handleClick = () => {
-    // GetVideoScript();
+    GetVideoScript();
 
     //testing
-    const fileUrl = "https://firebasestorage.googleapis.com/v0/b/ai-video-generator-e5638.appspot.com/o/ai-short-video%2Fc26779f3-89d9-491d-bb99-18d6ed2c5f45.mp3?alt=media&token=a4294a29-cd20-4c18-9bee-5e2caa7d9a8e"
-    GenerateAudioCaption(fileUrl)
+    // const fileUrl = "https://firebasestorage.googleapis.com/v0/b/ai-video-generator-e5638.appspot.com/o/ai-short-video%2Fc26779f3-89d9-491d-bb99-18d6ed2c5f45.mp3?alt=media&token=a4294a29-cd20-4c18-9bee-5e2caa7d9a8e"
+    // GenerateAudioCaption(fileUrl)
   };
 
   //Get Video Script
   const GetVideoScript = async () => {
-    setLoading(true);
-    const prompt = `write a script to generate ${formData.duration} video on topic : ${formData.topic} along with AI image prompt in ${formData.imageStyle} format for each scene and give me result in JSON format with imagePrompt and ContentText as field`;
-    console.log(prompt);
-    const result = await axios
-      .post("/api/get-video-script", { prompt })
-      .then((resp) => {
+    try {
+      setLoading(true);
+      const prompt = `write a script to generate ${formData.duration} video on topic : ${formData.topic} along with AI image prompt in ${formData.imageStyle} format for each scene and give me result in JSON format with imagePrompt and ContentText as field`;
+      console.log(prompt);
+      const resp = await axios.post("/api/get-video-script", { prompt });
+      if (resp?.data?.result) {
+        setVideoData(prev => ({
+          ...prev,
+          'videoScript': resp.data.result
+        }))
         setVideoScript(resp.data.result);
-        GenerateAudioFile(resp.data.result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    setLoading(false);
+        await GenerateAudioFile(resp.data.result);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+      setLoading(false);
+    }
   };
 
   //Generate audio file
   const GenerateAudioFile = async (videoScriptData) => {
-    setLoading(true);
-    let script = "";
-    const id = uuidv4();
-    videoScriptData.forEach((item) => {
-      script = script + item.ContentText + " ";
-    });
-    await axios
-      .post("/api/generate-audio", { text: script, id: id })
-      .then((resp) => {
-        setAudioFileUrl(resp.data.result)
-      })
-      .catch((error) => {
-        console.log(error);
+    try {
+      setLoading(true);
+      let script = "";
+      const id = uuidv4();
+      videoScriptData.forEach((item) => {
+        script = script + item.ContentText + " ";
       });
+      const resp = await axios.post("/api/generate-audio", {
+        text: script,
+        id: id,
+      });
+      if (resp?.data?.result) {
+        setVideoData(prev => ({
+          ...prev,
+          'audioFileUrl': resp.data.result
+        }))
+        setAudioFileUrl(resp.data.result);
+        await GenerateAudioCaption(resp.data.result, videoScriptData);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+      setLoading(false);
+    }
+  };
+
+  //Generate audio caption
+  const GenerateAudioCaption = async (fileUrl, videoScriptData) => {
+    try {
+      setLoading(true);
+      const resp = await axios.post("/api/generate-caption", {
+        audioFileUrl: fileUrl,
+      });
+      if (resp?.data?.result) {
+        setVideoData(prev => ({
+          ...prev,
+          'captions': resp.data.result
+        }))
+        setCaptions(resp.data.result);
+        GenerateImage(videoScriptData);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+      setLoading(false);
+    }
+  };
+
+  const GenerateImage = async (videoScriptData) => {
+    let images = [];
+    for(const element of videoScriptData){
+      try {
+        const resp = await axios.post("/api/generate-image", {
+          prompt: element.imagePrompt
+        });
+        if(resp.data.result){
+          console.log(resp.data.result);
+          images.push(resp.data.result);
+          setVideoData(prev => ({
+            ...prev,
+            'imageList': images
+          }))
+        }
+      } catch (error) {
+        console.log("error: ",error);
+      }
+    }
+    setImageList(images);
     setLoading(false);
   };
 
-  //Generate audio caption 
-  const GenerateAudioCaption = async (fileUrl) => {
-    setLoading(true);
-
-    await axios.post("/api/generate-caption", {audioFileUrl: fileUrl}).then(resp => {
-      setCaptions(resp.data.result);
-    }).catch(error => {
-      console.log(error);
-    })
-    setLoading(false);
-  }
+  useEffect(() => {
+    console.log("videoData: ", videoData);
+  }, [videoData])
 
   return (
     <div className="md:px-20">
